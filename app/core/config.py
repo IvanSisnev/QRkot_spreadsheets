af0181021e9b2de4502e4d5288383fd4acf6496d
Иван Сиснёв <ivan.sisnev@gmail.com>
2023-10-24 18:13:43 +0300

    Настроить обработку учетных данных и создать сессию Aiogoogle

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,7 @@
 """
 Конфигурация приложения.
 """
+from typing import Optional
 from pydantic import BaseSettings
 
 
@@ -9,6 +10,24 @@ class Settings(BaseSettings): # noqa
     app_title: str = 'QRKot'
     app_description: str = 'Приложение для Благотворительного фонда QRKot'
     secret: str = 'secret'
+
+    # Переменные Google API
+    type: Optional[str] = None
+    project_id: Optional[str] = None
+    private_key_id: Optional[str] = None
+    private_key: Optional[str] = None
+    client_email: Optional[str] = None
+    client_id: Optional[str] = None
+    auth_uri: Optional[str] = None
+    token_uri: Optional[str] = None
+    auth_provider_x509_cert_url: Optional[str] = None
+    client_x509_cert_url: Optional[str] = None
+    email: Optional[str] = None
+    # уровни доступа Google API
+    scopes = [
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive'
+    ]
 
     class Config: # noqa
         env_file = '.env'
